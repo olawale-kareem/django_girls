65d412c1469648d62f485eeeb3515b16227d7a70
olawale-kareem <olawale.kareem@decagon.dev>
2021-09-02 14:04:25 +0100

    all functionalities completed

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,15 @@ from .views import *
 
 urlpatterns = [
     path('', home, name = 'home'),
-    path('posts/', post_list, name = 'posts'),
+    
+    path('posts/', post_list, name = 'post_list'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+    path('post/<pk>/publish/', post_publish, name='post_publish'),
+    path('post/<pk>/remove/', post_remove, name='post_remove'),
+
     path('post/new/', post_new, name='post_new'),
+    path('drafts/', post_draft_list, name='post_draft_list'),
 ]
 
 
