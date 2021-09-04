12e3a209bba77e9f606991a30b7ddaec40d85716
olawale-kareem <olawale.kareem@decagon.dev>
2021-09-04 16:07:03 +0100

    few change on implementation

--- django_girls/settings.py
+++ django_girls/settings.py
@@ -105,8 +105,8 @@ if os.environ.get('GITHUB_WORKFLOW'):
         }
     }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
